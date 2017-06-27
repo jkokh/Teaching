d61c26b0a68a99f6e5360ecfd6ff60eb58261861
Evgeny Kokhanovsky <evgeny.kokhanovsky@gmail.com>
2017-06-27 13:58:52 +0300

    Changed about page

--- views/Teaching/content/about.php
+++ views/Teaching/content/about.php
@@ -1,5 +1,4 @@
 <div id='content'>
 	<h1>About <?php echo Users::communityName() ?></h1>
-	Here you can write something about this app or website.<br>
-	Or if you want, you can remove this page.
+	The coolest about page.
 </div>
