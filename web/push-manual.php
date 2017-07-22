6ecccbd32065b16aedf10ca0c183e87bd201abd7
Evgeny Kokhanovsky <evgeny.kokhanovsky@gmail.com>
2017-07-22 23:41:56 +0300

    PHP manual push

---
+++ web/push-manual.php
@@ -0,0 +1,16 @@
+<?php
+include(dirname(__FILE__).DIRECTORY_SEPARATOR.'Q.inc.php');
+
+$devices = Users_Device::select('*')->fetchDbRows();
+
+foreach($devices as $device) {
+	$device::$subscription = $device->fields['deviceId'];
+	$device->handlePushNotification([
+		'alert' => [
+			'title' => 'Hello ' . $device->fields['platform'],
+			'body' => 'PHP notifications'
+		],
+		'icon' => "https://cdn4.iconfinder.com/data/icons/logos-3/504/php-128.png"
+	]);
+	$device->sendPushNotifications();
+}
